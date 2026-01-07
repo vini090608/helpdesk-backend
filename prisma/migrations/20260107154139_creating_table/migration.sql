20ba11513ec63fa50e59bce5c58586a1f43166fe
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-01-07 12:42:10 -0300

    Creating DB and using Docker, Prisma

---
+++ prisma/migrations/20260107154139_creating_table/migration.sql
@@ -0,0 +1,69 @@
+-- CreateEnum
+CREATE TYPE "BusinessHour" AS ENUM ('H08', 'H09', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17');
+
+-- CreateEnum
+CREATE TYPE "CallStatus" AS ENUM ('aberto', 'emAtendimento', 'encerrado');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('cliente', 'admin');
+
+-- CreateTable
+CREATE TABLE "technicals" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "hour" "BusinessHour"[],
+
+    CONSTRAINT "technicals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'cliente',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "services" (
+    "name" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "calls" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "describe" TEXT NOT NULL,
+    "status" "CallStatus" NOT NULL,
+    "serviceAmount" DOUBLE PRECISION NOT NULL,
+    "client_id" INTEGER NOT NULL,
+    "technical_id" INTEGER NOT NULL,
+    "service_name" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "calls_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "technicals_email_key" ON "technicals"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "services_name_key" ON "services"("name");
+
+-- AddForeignKey
+ALTER TABLE "calls" ADD CONSTRAINT "calls_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "calls" ADD CONSTRAINT "calls_technical_id_fkey" FOREIGN KEY ("technical_id") REFERENCES "technicals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "calls" ADD CONSTRAINT "calls_service_name_fkey" FOREIGN KEY ("service_name") REFERENCES "services"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express"
+import "express-async-errors"
+
+import { errorHandling } from "@/middlewares/error-handling"
 
 const app = express()
+
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!")
-})
+app.use(errorHandling)
 
-
-export { app }
+export {app}
