626c9ea3b0e272a0201e3ca4d528d7d3f8da8c69
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-01-20 13:02:57 -0300

    Configuring cors in page

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express from "express"
 import "express-async-errors"
 
+import cors from "cors"
 import { routes } from "./routes"
 import { errorHandling } from "@/middlewares/error-handling"
 
 const app = express()
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 
 app.use(express.json())
 app.use(routes)
+
+
+
 
 app.use(errorHandling)
 
